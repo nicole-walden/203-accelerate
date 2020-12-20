56bf175d323dadabc9f99e07eeb17c07d3ae5c66
Nicole Walden <walden.nicole.c@gmail.com>
2020-12-20 14:26:36 -0800

    contact us page styles and media queries

--- wp-content/themes/accelerate-theme-child/page.php
+++ wp-content/themes/accelerate-theme-child/page.php
@@ -14,12 +14,14 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div class="contact-us">
-			<?php while ( have_posts() ) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<div class="contact-us">
+				<?php while ( have_posts() ) : the_post(); ?>
+	        <h2><?php the_title(); ?></h2>
+	        <?php the_content(); ?>
+				<?php endwhile; // end of the loop. ?>
+			</div>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
