8830903b37f9a373806c08dbc19c2c663a04be08
Nicole Walden <walden.nicole.c@gmail.com>
2020-12-20 13:46:27 -0800

    about page styles and media queries

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -10,8 +10,8 @@
 
 get_header(); ?>
 
-<div id="primary" class="home-page hero-content">
-	<div class="about" role="main">
+<div id="primary" class="home-page hero-content about">
+	<div class="main-content" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php the_content(); ?>
 		<?php endwhile; // end of the loop. ?>
@@ -20,18 +20,23 @@ get_header(); ?>
 
 <!-- SERVICES -->
 	<section class="services">
-
 		<div class="site-content">
-			<div class="about-intro">
-				<h4>Our Services</h4>
-				<p>We take pride in our clients and the content we create for them. <br>Here's a brief overview of our offered services.</p>
+
+			<div class="about-subheading">
+				<?php while ( have_posts() ) : the_post();
+					$subheading = get_field("subheading");
+					$description = get_field("description");
+				?>
+					<h4><?php echo $subheading ?></h4>
+					<p><?php echo $description ?></p>
+				<?php endwhile; // end of the loop ?>
 			</div>
 
 			<ul class="about-services">
 				<?php query_posts('posts_per_page=4&post_type=services'); ?>
 				<?php while ( have_posts() ) : the_post();
-						$service_icon = get_field("service_icon");
-						$size = "medium";
+					$service_icon = get_field("service_icon");
+					$size = "medium";
 				?>
 
 				<li class="individual-services">
@@ -50,9 +55,13 @@ get_header(); ?>
 		</div>
 	</section>
 
-	<div class="contact-us-button">
-    <h2>Interested in working with us?</h2>
-	  <a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+	<section class="about-cta">
+		<div class="site-content">
+			<div class="contact-us-button">
+		    <h2>Interested in working with us?</h2>
+			  <a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+			</div>
+		</div>
 	</div>
 
 <?php get_footer(); ?>
