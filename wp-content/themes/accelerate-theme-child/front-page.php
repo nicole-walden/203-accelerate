e689e067c9284d76f05740a5df26fdcc21b5eec3
Nicole Walden <walden.nicole.c@gmail.com>
2020-12-20 10:21:28 -0800

    homepage styles and media queries

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -2,11 +2,6 @@
 /**
  * The template for displaying the homepage
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
  * @package WordPress
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 2.0
@@ -25,15 +20,14 @@ get_header(); ?>
 
 <!-- FEATURED WORK -->
 	<section class="featured-work">
-
 		<div class="site-content">
 			<h4>Featured Work</h4>
 
 			<ul class="homepage-featured-work">
 				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 				<?php while ( have_posts() ) : the_post();
-						$image_1 = get_field("image_1");
-						$size = "medium";
+					$image_1 = get_field("image_1");
+					$size = "medium";
 				?>
 
 				<li class="individual-featured-work">
@@ -49,32 +43,55 @@ get_header(); ?>
 		</div>
 	</section>
 
-	<!-- RECENT BLOG POST -->
-	<section class="recent-posts">
+	<!-- OUR SERVICES -->
+	<section class="our-services">
 		<div class="site-content">
-			<div class="blog-post">
-				<h4>From the Blog</h4>
-					<?php query_posts('posts_per_page=1'); ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-						<h3><?php the_title(); ?></h3>
-						<?php the_excerpt(); ?>
-					<?php endwhile; // end of the loop ?>
-					<?php wp_reset_query(); // resets the altered query back to the original ?>
-			</div>
+			<h4>Our Services</h4>
+
+			<ul class="homepage-our-services">
+				<?php query_posts('posts_per_page=4&post_type=services'); ?>
+				<?php while ( have_posts() ) : the_post();
+					$service_icon = get_field("service_icon");
+					$size = "medium";
+				?>
+
+				<li class="homepage-individual-services">
+					<figure>
+						<?php echo wp_get_attachment_image($service_icon, $size); ?>
+					</figure>
+					<h3><?php echo the_title(); ?></h3>
+				</li>
+
+				<?php endwhile; // end of the loop ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</ul>
 		</div>
 	</section>
 
-	<!-- DYNAMIC SIDEBAR -->
-	<section class="recent-tweets">
-		<div class="tweet">
-			<h4>Recent Tweet</h4>
-			<h3>@Accelerate</h3>
-			<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-			<div id="secondary" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-2' ); ?>
-				<p class="read-more-link"><a href="http://www.twitter.com">Follow Us &rsaquo;</a></p>
+	<!-- RECENT ITEMS -->
+	<section class="recent-items">
+		<div class="site-content">
+
+			<div class="blog-post"> <!-- BLOG POST -->
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<?php the_excerpt(); ?>
+				<?php endwhile; // end of the loop ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
 			</div>
-			<?php endif; ?>
+
+			<div class="blog-post"> <!-- DYNAMIC SIDEBAR -->
+				<h4>Recent Tweet</h4>
+				<h3>@Accelerate</h3>
+				<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+				<div id="secondary" class="widget-area" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					<p class="read-more-link"><a href="http://www.twitter.com">Follow Us &rsaquo;</a></p>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
 	</section>
 
--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -10,29 +10,49 @@
 
 get_header(); ?>
 
-	<div id="primary" class="home-page hero-content">
-		<div id="content" role="main">
+<div id="primary" class="home-page hero-content">
+	<div class="about" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
 
-        <?php while ( have_posts() ) : the_post();
-          $service_icon = get_field("service_icon");
-          $size = "medium";
-        ?>
+<!-- SERVICES -->
+	<section class="services">
 
-        <article class="services clearfix">
-          <div class="services-info">
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-          </div>
+		<div class="site-content">
+			<div class="about-intro">
+				<h4>Our Services</h4>
+				<p>We take pride in our clients and the content we create for them. <br>Here's a brief overview of our offered services.</p>
+			</div>
 
-          <div class="services-icon">
-            <?php if($service_icon) {
-    				  echo wp_get_attachment_image( $service_icon, $size );
-            } ?>
-    			</div>
-        </article>
+			<ul class="about-services">
+				<?php query_posts('posts_per_page=4&post_type=services'); ?>
+				<?php while ( have_posts() ) : the_post();
+						$service_icon = get_field("service_icon");
+						$size = "medium";
+				?>
 
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+				<li class="individual-services">
+					<figure class="individual-services-icon">
+						<?php echo wp_get_attachment_image($service_icon, $size); ?>
+					</figure>
+					<div class="individual-services-text">
+						<h3><?php the_title(); ?></h3>
+						<?php the_content(); ?>
+					</div>
+				</li>
+
+				<?php endwhile; // end of the loop ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</ul>
+		</div>
+	</section>
+
+	<div class="contact-us-button">
+    <h2>Interested in working with us?</h2>
+	  <a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+	</div>
 
 <?php get_footer(); ?>
